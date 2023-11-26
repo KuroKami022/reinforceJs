a6055fdbea9e771105637bacac62f7e500261987
Kami <luisru693@gmail.com>
2023-11-26 06:46:11 -0500

    mutability and inmutability inside Js

---
+++ jscomplexdata/variable.js
@@ -0,0 +1,11 @@
+//not permited
+let cajaDeAndy='Woody'
+console.log(cajaDeAndy)
+let c='Woody'
+let cda='Woody'
+let pcAndy='Woody'
+
+//permited
+let primerTrasteoDeAndy='Woody'
+let urlDelUsuario='https://www.google.com'
+let idDelUsuario='123456789'
