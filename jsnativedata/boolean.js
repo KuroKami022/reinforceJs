f611aaac9ef4378796b5ac4fb5707fb494f07228
Kami <luisru693@gmail.com>
2023-11-27 10:49:31 -0500

    to handle data type boolean

---
+++ jsnativedata/boolean.js
@@ -0,0 +1,15 @@
+const isActive = true
+const hasPermisson = false
+
+//implicit conversion
+const result = 5 > 3
+console.log(result)
+const name = 'Platzi'
+console.log(!!name)
+
+//explicit conversion
+const value = 0
+const otherValue = -24
+const explicittBoolean = Boolean(value)
+const explicitBoolean = Boolean(otherValue)
+console.log(explicitBoolean)
