251313e69da24c609341cc31d821a6dcdc5ff2a5
Kami <luisru693@gmail.com>
2023-11-27 19:08:44 -0500

    conversion types

---
+++ typeConversion.js
@@ -0,0 +1,48 @@
+// explicit type case
+
+//number
+const string = '42'
+const integer = parseInt(string)
+
+console.log(integer);
+console.log(typeof integer);
+
+//decimal
+const stringDecimal = '3.14'
+const float = parseFloat(stringDecimal)
+console.log(float);
+console.log(typeof float);
+
+//binary to decimal
+const binary = '1010'
+const decimal = parseInt(binary, 2)
+console.log(decimal);
+console.log(typeof decimal);
+
+
+// implicit type
+
+const sum = '5' + 3
+console.log(sum);
+
+const sumWithBoolean = '3' + true
+console.log(sumWithBoolean);
+
+const sumWithNumber = 2 + true
+console.log(sumWithNumber);
+
+const stringValue = '10'
+const numberValue = 10
+const booleanValue = true
+
+//combinations variate from concatenation to sum on a jerarchic order
+
+console.log(stringValue + stringValue);
+console.log(stringValue + numberValue);
+console.log(stringValue + booleanValue);
+console.log(numberValue + stringValue);
+console.log(numberValue + numberValue);
+console.log(numberValue + booleanValue);
+console.log(booleanValue + stringValue);
+console.log(booleanValue + numberValue);
+console.log(booleanValue + booleanValue);
