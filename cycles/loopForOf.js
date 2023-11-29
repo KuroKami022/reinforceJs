98e698cf8b47c839e95b6ff040b782c854736a31
Kami <luisru693@gmail.com>
2023-11-29 05:50:22 -0500

    For Of Loop

---
+++ cycles/loopForOf.js
@@ -0,0 +1,11 @@
+// works on arrays or strings
+
+// for (const iterator of object) {
+// codigo a ejecutar
+// }
+
+let basket = ["apple", "orange", "pear", "grape"]
+
+for (fruits of basket) {
+    console.log(fruits);
+}
